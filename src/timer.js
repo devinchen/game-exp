3b1d628b93ed37c69a030f25c0499fae350ce03a
devinchen <deseze74@gmail.com>
2016-05-22 02:22:26 +0800

    Refactor timer

--- src/timer.js
+++ src/timer.js
@@ -2,10 +2,31 @@ var Timer = (function() {
   var instance;
 
   function createInstance() {
+    var countdown;
+
     return {
       font: '20px Arial',
       fontColor: 'blue',
       time: 0,
+
+      start: function() {
+        countdown = setInterval(function() {
+          this.time -= 1;
+
+          if (this.time === 0) {
+            clearInterval(countdown);
+          }
+        }.bind(this), 1000);
+      },
+
+      pause: function() {
+        clearInterval(countdown);
+      },
+
+      resume: function() {
+        this.start();
+      },
+
       draw: function(context) {
         context.save();
         context.font = this.font;
