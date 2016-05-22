438a9ab4bbdd8702a580434b470b13125c988f33
devinchen <deseze74@gmail.com>
2016-05-23 03:45:41 +0800

    Generate texture

--- src/animal.js
+++ src/animal.js
@@ -1,7 +1,7 @@
 function Animal() {
   this.x = 0;
   this.y = 0;
-  this.label = '';
+  this.texture = '';
   this.speed = Math.random() * 10;
   this.isStop = false;
 }
@@ -19,6 +19,12 @@ Animal.prototype.stop = function() {
   this.isStop = true;
 };
 
+Animal.prototype.setTexture = function(texture) {
+  var image = new Image();
+  image.src = texture;
+  this.texture = image;
+};
+
 Animal.prototype.draw = function(context) {
   if (this.isOutsideBoundary(context)) {
     this.speed *= -1;
@@ -29,8 +35,8 @@ Animal.prototype.draw = function(context) {
   context.save();
   context.translate(this.x, this.y);
   context.fillStyle = 'gray';
-  context.fillText(this.label, this.x, this.y);
-  context.fillRect(0,0,25,25);
+  context.scale(0.5, 0.5);
+  context.drawImage(this.texture, 0, 0);
   context.restore();
 };
 
---
+++ src/textures.js
@@ -0,0 +1,16 @@
+function getTextures() {
+  return [
+    'src/images/0.png',
+    'src/images/1.png',
+    'src/images/2.png',
+    'src/images/3.png',
+    'src/images/4.png',
+    'src/images/5.png',
+    'src/images/6.png',
+    'src/images/7.png',
+    'src/images/8.png',
+    'src/images/9.png',
+    'src/images/10.png',
+    'src/images/11.png',
+  ];
+}
