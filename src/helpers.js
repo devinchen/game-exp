1e0b084506974ee397b8525e757ae614f0e085d9
devinchen <deseze74@gmail.com>
2016-05-25 00:59:23 +0800

    Refactoring

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,25 @@
 function getRandomPosition(context) {
   var maxWidth = context.canvas.width;
   var maxHeight = context.canvas.height;
+  var randomX = Math.round(maxWidth * Math.random());
+  var randomY = Math.round(maxHeight * Math.random());
+  var borderSize = 15;
+  var animalSize = 50;
+
+  if (randomX < borderSize) {
+    randomX = borderSize;
+  } else if (randomX > maxWidth - borderSize - animalSize) {
+    randomX = maxWidth - borderSize - animalSize;
+  }
+
+  if (randomY < borderSize) {
+    randomY = borderSize;
+  } else if (randomY > maxHeight - borderSize - animalSize) {
+    randomY = maxHeight - borderSize - animalSize;
+  }
 
   return {
-    x: Math.round((maxWidth - 60) * Math.random()),
-    y: Math.round((maxHeight - 60) * Math.random()),
+    x: randomX,
+    y: randomY,
   };
 }
